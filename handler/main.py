50368c78d803f9300b02f9b0e36229fde36c9ba5
Johnny0710 <920710718@qq.com>
2018-10-03 10:51:43 +0800

    Add Base Handler

--- handler/main.py
+++ handler/main.py
@@ -1,16 +1,19 @@
 import tornado.web
 
-class IndexHandler(tornado.web.RequestHandler):
+class BaseHandler(tornado.web.RequestHandler):
+    pass
+
+class IndexHandler(BaseHandler):
     def get(self):
         self.render("index.html")
 
-class ExploerHandler(tornado.web.RequestHandler):
+class ExploerHandler(BaseHandler):
     def get(self, *args, **kwargs):
         self.render('exploer.html')
 
-class PostHandler(tornado.web.RequestHandler):
+class PostHandler(BaseHandler):
     def get(self, post_id, *args, **kwargs):
         self.render('post.html',post_id=post_id)
 
-class LoginHandler(tornado.web.RequestHandler):
+class LoginHandler(BaseHandler):
     pass
