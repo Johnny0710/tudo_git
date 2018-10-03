285ba03f05767704cf1ef5b578485a8556d414a1
Johnny0710 <920710718@qq.com>
2018-10-03 22:49:02 +0800

    添加了expoler的缩略图展示 后续如有新功能发现,待调整

--- handler/main.py
+++ handler/main.py
@@ -1,7 +1,7 @@
 import tornado.web
 import  os
 
-
+from utils import img
 
 class BaseHandler(tornado.web.RequestHandler):
     pass
@@ -15,14 +15,16 @@ class ExploerHandler(BaseHandler):
     图片缩略图
     """
     def get(self, *args, **kwargs):
-        self.render('exploer.html',files = os.listdir('./statics/uploads'))
+        self.render('exploer.html',files = os.listdir('./statics/uploads/thumbnail'))
 
 class PostHandler(BaseHandler):
     """
     获取图片原图
     """
     def get(self, post_id, *args, **kwargs):
-        self.render('post.html',post_id=post_id)
+        post_id_tr = post_id.replace('_200x200','')
+        print(post_id_tr)
+        self.render('post.html',post_id=post_id_tr)
 
 class LoginHandler(BaseHandler):
     pass
@@ -36,5 +38,6 @@ class UploadFileHandler(BaseHandler):
         self.render('upload.html')
     def post(self, *args, **kwargs):
         res = self.request.files.get('newimg',None)
-        with open('statics/uploads/{}'.format(res[0].filename),'wb') as img_file:
+        with open('./statics/uploads/{}'.format(res[0].filename),'wb') as img_file:
             img_file.write(res[0].body)
+            img.create_thum('./statics/uploads/{}'.format(res[0].filename))
--- utils/img.py
+++ utils/img.py
@@ -1,12 +1,28 @@
 import os
+from PIL import Image
 
 def get_images(path):
     """
     获取提供的文件夹下的所有图片
     :param path:
-    :return:
+    :return:wir
     """
     files = os.listdir(path)
     return files
+
+# def create_thum(files_path):
+#     files = os.listdir(files_path)
+#     for file in files:
+#         img_split = file.split('.')
+#         img = Image.open('{}/{}'.format(files_path,file))
+#         img.thumbnail((200,200))
+#         print('{}thumbnail/{}_200x200.{}'.format(files_path,img_split[0],img_split[1]))
+def create_thum(file_path):
+    path = file_path.split(r'/')
+    img_split = path[-1].split('.')
+    path = r'/'.join(path[0:-1])
+    img = Image.open(file_path)
+    img.thumbnail((200,200))
+    img.save('{}/thumbnail/{}_200x200.{}'.format(path,img_split[0],img_split[1]))
 
 
